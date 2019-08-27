50c101a3fdaa5e39d30e61787ebb09c7a6195558
Enrique Khai <nigelotan00@gmail.com>
2019-08-27 19:49:38 +0800

    Level 2: Add, List.
    Learnt how to initialise an array of type String. (= new String[100])
    Used for-loop again.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,16 +17,33 @@ public class Duke {
 		System.out.println("Hello, I'm Duke.");
 		System.out.println("What can I do for you?");
 
+		int currIndex = 0;
+		String[] userInputs = new String[100];
 		String bye = "bye";
+		String bye1 = "list";
 		Scanner sc = new Scanner(System.in);
 		String userInput = sc.nextLine();
 
 		while (!bye.equals(userInput)) {
+			if (bye1.equals(userInput)) {
+				System.out.println("       __________________________________________");
+				for (int i = 0; i < currIndex; i++) {
+					int j = i + 1;
+					System.out.println("       " + j + ". " + userInputs[i]);
+				}				
+				System.out.println("       __________________________________________");
+				userInput = sc.nextLine();
+				continue;
+			}
+
 			System.out.println("       __________________________________________");
-			System.out.println("       " + userInput);
+			System.out.println("       added: " + userInput);
 			System.out.println("       __________________________________________");
+			userInputs[currIndex] = userInput;
+			currIndex++;
 			userInput = sc.nextLine();
 		}
+
 
 		System.out.println("       __________________________________________");
 		System.out.println("       Bye. Hope to see you again soon!");
