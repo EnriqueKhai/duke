708ccba6f191ad3cee5b560c511d02139fd355a6
Enrique Khai <nigelotan00@gmail.com>
2019-08-27 19:05:26 +0800

    Level 1: Greet, Echo, Exit.
    Made changes to Duke.class by importing the Scanner class.
    Learnt how to use the Scanner class to take user input.
    Do not forget "System.in" after instantiating a Scanner object.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Javadoc summary... ?
  */
 public class Duke {
-    /**
-     * Javadoc comment... ? 
-     */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+	/**
+	 * Javadoc comment... ? 
+	 */
+	public static void main(String[] args) {
+		String logo = " ____        _        \n"
+			+ "|  _ \\ _   _| | _____ \n"
+			+ "| | | | | | | |/ / _ \\\n"
+			+ "| |_| | |_| |   <  __/\n"
+			+ "|____/ \\__,_|_|\\_\\___|\n";
+		System.out.println("Hello from\n" + logo);
+		System.out.println("Hello, I'm Duke.");
+		System.out.println("What can I do for you?");
+
+		String bye = "bye";
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine();
+
+		while (!bye.equals(userInput)) {
+			System.out.println("       __________________________________________");
+			System.out.println("       " + userInput);
+			System.out.println("       __________________________________________");
+			userInput = sc.nextLine();
+		}
+
+		System.out.println("       __________________________________________");
+		System.out.println("       Bye. Hope to see you again soon!");
+		System.out.println("       __________________________________________");
+	}
 }
