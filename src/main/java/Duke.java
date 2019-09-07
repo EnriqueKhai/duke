3c6cd59f3297159ac7632d5a89e9ce98e71e009f
Enrique Khai <nigelotan00@gmail.com>
2019-09-07 11:28:26 +0800

    fixed text-ui-testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,33 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Duke {
+	protected static Scanner inputHistory;
+	protected static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		ui.printIntro();
 
-		File userHistory = new File("userHistory.txt");
 		//File userHistory = new File("C:/Users/Enrique Khai/Desktop/duke/src/main/java/userHistory.txt");
-
+		File userHistory = new File("userHistory.txt");
+		try {
+			inputHistory = new Scanner(userHistory);
+		} catch (IOException ex) {
+			System.out.println("ERROR: " + ex);
+		}
 		String[] userCommands = {"bye", "list"};
 		List<Task> userList = new ArrayList<Task>();
 
 
 		//loads userHistory.txt
-		try {
-			Scanner input = new Scanner(userHistory);
-			if (input.hasNextLine()) {
-				String userEntry = input.nextLine();
-				storage.loadData(userHistory, userCommands, userList, input, userEntry);
-			}
-		} catch (IOException ex) {
-			System.out.println("ERROR: " + ex);
+		if (inputHistory.hasNextLine()) {
+			String userEntry = inputHistory.nextLine();
+			storage.loadData(userHistory, userCommands, userList, inputHistory, userEntry);
 		}
 
 		//resumes from loaded user data
-		Scanner input = new Scanner(System.in);
 		String userEntry = input.nextLine();
 		Parser.processData(userHistory, userCommands, userList, input, userEntry);
 
 		ui.printOutro();
+
 	}
 }
---
+++ src/main/java/cp.sh
@@ -0,0 +1,3 @@
+cp input.txt ../../../text-ui-test/input.txt
+cp EXPECTED.txt ../../../text-ui-test/EXPECTED.txt
+cp userHistory.txt ../../../text-ui-test/userHistory.txt
